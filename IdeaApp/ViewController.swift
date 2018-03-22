83da1599af840051a1f2adc180c2b722d614934a
Hoang Thu Ha <hoangthuha880@gmail.com>
2018-03-22 23:19:09 +0700

    Initial Commit

---
+++ IdeaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdeaApp
+//
+//  Created by Hoang Thu Ha on 22/3/18.
+//  Copyright © 2018 Hoang Thu Ha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
